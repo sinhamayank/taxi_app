00a2aa2a8b5dc3eb7cf0fb1250c010adb7bbc96c
Mayank Sinha <mayanksinha.iitb@gmail.com>
2017-08-13 17:03:44 +0530

    added constraint so that a driver can accept only one ride at a time

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,6 +1,7 @@
 class DriversController < ApplicationController
 
   before_action :fetch_driver
+  before_action :check_if_ongoing_ride_present, only: [ :accept_ride ]
 
   def home
     @rides = Ride.fetch_waiting_and_driver_rides( @driver.id ) if @driver.present?
@@ -8,7 +9,7 @@ class DriversController < ApplicationController
 
   def accept_ride
     begin
-      if @driver.present?
+      if !@cant_accept
         ride = Ride.find_by_id( params[:ride_id] )
         if ride.status == "waiting"
           ride.update_attributes( driver_id: @driver.id, status: "ongoing", pickup_time: Time.now )
@@ -30,7 +31,17 @@ class DriversController < ApplicationController
 
   def fetch_driver
     @driver = Driver.find_by_sys_id( params[:id] )
-    flash[:error] = "Please give correct driver's id" if @driver.blank?
+    if @driver.blank?
+      @cant_accept = true
+      flash[:error] = "Please give correct driver's id"
+    end
+  end
+
+  def check_if_ongoing_ride_present
+    if ( @driver.present? and @driver.rides.ongoing.count > 0 )
+      @cant_accept = true
+      flash[:notice] = "Please complete your ongoing ride before accepting"
+    end
   end
 
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,6 +3,7 @@ class Ride < ActiveRecord::Base
   belongs_to :driver
 
   scope :waiting, -> { where(status: 'waiting') } #ideally the way status should be stored should be with some agreed convention
+  scope :ongoing, -> { where(status: 'ongoing') }
 
   def self.fetch_waiting_and_driver_rides( driver_id )
     Ride.select("id, customer_id, request_time, pickup_time, status").where("driver_id IS NULL OR driver_id = ?", driver_id).includes(:customer).group_by(&:status)
